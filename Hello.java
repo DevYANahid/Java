bfa291bbab0e7229e5dc663ad67d90bcce4db26d
DevYANahid <codealgorithmschool@gmail.com>
2024-08-01 23:45:01 +0600

    init

---
+++ Hello.java
@@ -0,0 +1,29 @@
+class Hello
+{
+    public static void main (String a[])
+    {
+
+
+        //Print 
+        int num1 = 3;
+        int num2 = 9;
+        int result = num1 + num2;
+        System.out.println("Hello World");
+        System.out.println(result);
+
+        //Data Type in Java
+        int number = 56;
+        byte b = 127;
+        short s = 658;
+        long l =4756l;
+
+        float f = 5.8f;
+        double d = 5.7;
+
+        char c = 'k';
+        boolean bool = true;
+
+        //Assignment Operators
+        
+    }
+}
