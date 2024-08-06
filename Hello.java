34bc12d69f82a609d72694cb75b0b79c5c1bf71c
DevYANahid <codealgorithmschool@gmail.com>
2024-08-06 22:10:02 +0600

    java for

--- Hello.java
+++ Hello.java
@@ -3,48 +3,21 @@ class Hello
     public static void main (String a[])
     {
 
+    //============WHILE==============//    
+        // int i=1;
 
-        //Print 
-        int num1 = 3;
-        int num2 = 9;
-        int result = num1 + num2;
-        System.out.println("Hello World");
-        System.out.println(result);
-
-        //Data Type in Java
-        int number = 56;
-        byte b = 127;
-        short s = 658;
-        long l =4756l;
-
-        float f = 5.8f;
-        double d = 5.7;
-
-        char c = 'k';
-        boolean bool = true;
-
-        //Assignment Operators
-
-
-        //while
-
-        
-        // int i = 1;
-
-        // while (5>=i)
-
-        
+        // while(i<=5)
         // {
-        //     System.out.println("HI" + i);
+        //     System.out.println("out" + i);
         //     i++;
         // }
-    
-    for(int i=1; i<=4;i++)
-    {
-            System.out.println( "hi" + i);
+
+
+        for(int i=1; i<=5; i++)
+            {
+                System.out.println("out" + i);
+            }
     }
-    }
-    
 }
 
 
