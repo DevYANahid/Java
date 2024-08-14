28cf78a2ac49fb8bdcddba0f148cba21bf760326
DevYANahid <codealgorithmschool@gmail.com>
2024-08-14 12:26:54 +0600

    java OOPS

---
+++ OOPS.java
@@ -0,0 +1,37 @@
+
+    //*****************OOPS*********************************//
+
+    class Pen {
+      String color;
+      String type;
+
+      public void write()
+      {
+        System.out.println("write anything");
+      } 
+
+      public void printColor(){
+        System.out.println(this.color);
+      };
+    }
+
+
+    public class OOPS{
+      public static void main(String args[]) {
+          Pen pen1 = new Pen();
+          pen1.color ="red";
+          pen1.type = "gel";
+
+
+          Pen pen2 = new Pen();
+          pen2.color="green";
+          pen2.type="point";
+
+
+
+
+     
+          pen1.printColor();
+          pen2.printColor();
+      }
+    }
