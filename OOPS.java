0610ce052af5871998d9f5c8165427626be663a8
DevYANahid <codealgorithmschool@gmail.com>
2024-08-14 13:12:31 +0600

    java OOPS

--- OOPS.java
+++ OOPS.java
@@ -19,7 +19,7 @@
     public class OOPS{
       public static void main(String args[]) {
           Pen pen1 = new Pen();
-          pen1.color ="red";
+          pen1.color ="blue";
           pen1.type = "gel";
 
 
